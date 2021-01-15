108a5a0776f6b719a09d2c1d8906444f4ea9b321
Vitaliy <theyamshikov@yandex.ru>
2021-01-15 19:16:28 +0600

    upd

--- src/Domain/Facades/Bot.php
+++ src/Domain/Facades/Bot.php
@@ -2,36 +2,41 @@
 
 namespace ZnLib\Telegram\Domain\Facades;
 
-use ZnLib\Telegram\Domain\Facades\BotFacade;
-use ZnTool\Dev\Dumper\Domain\Repositories\Telegram\DumperRepository;
-
 class Bot
 {
 
-    private static $responseService;
+    private static $responseService = null;
+    private static $chatId = null;
 
-    public static function dump($message, $chatId = null, bool $isEncode = true)
+    public static function init(string $token, int $chatId = null)
+    {
+        self::$chatId = $chatId;
+        self::$responseService = BotFacade::getResponseService($token);
+    }
+
+    public static function dump($message, int $chatId = null, bool $isEncode = true)
     {
         self::sendMessage($message, $chatId, $isEncode);
     }
 
-    public static function send($message, $chatId = null, bool $isEncode = true)
+    public static function send($message, int $chatId = null, bool $isEncode = true)
     {
         self::dump($message, $chatId, $isEncode);
     }
 
-    public static function sendAsString($message, $chatId = null)
+    public static function sendAsString($message, int $chatId = null)
     {
         self::sendMessage($message, $chatId, false);
     }
 
-    public static function sendMessage($messageData, $chatId = null, bool $isEncode = true)
+    public static function sendMessage($messageData, int $chatId = null, bool $isEncode = true)
     {
         if ($isEncode) {
             $messageText = json_encode($messageData, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         } else {
             $messageText = $messageData;
         }
+        $chatId = $chatId ?: self::$chatId;
         $chatId = $chatId ?: $_ENV['DUMPER_BOT_ADMIN_ID'];
         if (empty(self::$responseService)) {
             self::$responseService = BotFacade::getResponseService($_ENV['DUMPER_BOT_TOKEN']);

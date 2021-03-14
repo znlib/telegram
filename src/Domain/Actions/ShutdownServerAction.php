f96d7f3370db148d4b426606960e271f5c14c65b
Vitaliy <theyamshikov@yandex.ru>
2021-03-14 19:21:09 +0600

    upd

--- src/Domain/Actions/ShutdownServerAction.php
+++ src/Domain/Actions/ShutdownServerAction.php
@@ -4,25 +4,35 @@ namespace ZnLib\Telegram\Domain\Actions;
 
 use danog\MadelineProto\APIFactory;
 use danog\MadelineProto\EventHandler;
+use ZnCore\Base\Enums\OsFamilyEnum;
+use ZnCore\Base\Helpers\OsHelper;
 use ZnLib\Telegram\Domain\Base\BaseAction;
 use ZnLib\Telegram\Domain\Entities\MessageEntity;
+use ZnLib\Telegram\Domain\Entities\RequestEntity;
 
 class ShutdownServerAction extends BaseAction
 {
 
-    private $eventHandler;
+    /*private $eventHandler;
 
     public function __construct(EventHandler $eventHandler)
     {
         parent::__construct();
         $this->eventHandler = $eventHandler;
-    }
+    }*/
 
-    public function run(MessageEntity $messageEntity)
+    public function run(RequestEntity $requestEntity)
     {
+        //return $this->response->sendMessage($requestEntity->getMessage()->getChat()->getId(), $this->text);
         // powercfg /hibernate off
         //shell_exec('rundll32.exe powrprof.dll,SetSuspendState 0,1,0');
-        shell_exec('Rundll32.exe powrprof.dll,SetSuspendState 0');
+        if(OsHelper::isFamily(OsFamilyEnum::LINUX)) {
+            shell_exec('systemctl suspend');
+        } else {
+            shell_exec('Rundll32.exe powrprof.dll,SetSuspendState 0');
+        }
+
+
     }
 
 }
--- src/Domain/Matchers/IsAdminMatcher.php
+++ src/Domain/Matchers/IsAdminMatcher.php
@@ -2,18 +2,24 @@
 
 namespace ZnLib\Telegram\Domain\Matchers;
 
+use ZnLib\Telegram\Domain\Entities\RequestEntity;
+use ZnLib\Telegram\Domain\Helpers\MatchHelper;
 use ZnLib\Telegram\Domain\Interfaces\MatcherInterface;
 
 class IsAdminMatcher implements MatcherInterface
 {
 
-    public function isMatch(array $update): bool
+    public function isMatch(RequestEntity $requestEntity): bool
     {
-		if(empty($update['message']['from_id']) || empty($update['message']['to_id']['user_id'])) {
+        $message = $requestEntity->getMessage()->getText();
+        $fromId = $requestEntity->getMessage()->getFrom()->getId();
+        $toId = $requestEntity->getMessage()->getChat()->getId();
+        
+		if(empty($fromId) || empty($toId)) {
 			return false;
 		}
-        $isSelf = $update['message']['from_id'] == $update['message']['to_id']['user_id'];
-        $isAdmin = $update['message']['from_id'] == $_ENV['ADMIN_ID'];
+        $isSelf = $fromId == $toId;
+        $isAdmin = $fromId == $_ENV['ADMIN_ID'];
         return $isSelf || $isAdmin;
     }
 

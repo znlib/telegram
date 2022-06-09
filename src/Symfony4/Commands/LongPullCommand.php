7946152bd16bd8df7a97cb76cf43188d06d4d373
ZnDeveloper <zndev@ya.ru>
2022-06-10 00:48:35 +0600

    upd

--- src/Symfony4/Commands/LongPullCommand.php
+++ src/Symfony4/Commands/LongPullCommand.php
@@ -3,22 +3,23 @@
 namespace ZnLib\Telegram\Symfony4\Commands;
 
 use Psr\Container\ContainerInterface;
-use React\EventLoop\Loop;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Lock\LockFactory;
 use ZnCore\Base\Libs\App\Helpers\EnvHelper;
 use ZnCore\Base\Libs\Container\Traits\ContainerAwareTrait;
+use ZnLib\Console\Symfony4\Traits\LockTrait;
+use ZnLib\Console\Symfony4\Traits\LoopTrait;
 use ZnLib\Telegram\Domain\Repositories\File\ConfigRepository;
 use ZnLib\Telegram\Domain\Services\LongPullService;
-use ZnLib\Console\Symfony4\Traits\LockTrait;
 
 class LongPullCommand extends Command
 {
 
     use ContainerAwareTrait;
     use LockTrait;
+    use LoopTrait;
 
     protected static $defaultName = 'telegram:long-pull';
     protected $longPullService;
@@ -43,23 +44,15 @@ class LongPullCommand extends Command
     {
         $output->writeln('<fg=white># Long pull</>');
         $output->writeln('');
+
+        $output->writeln('<fg=white>timeout:</> <fg=yellow>' . $this->configRepository->getLongpullTimeout() . ' second</>');
         $name = 'telegramBot.longPull';
         $this->runProcessWithLock($input, $output, $name);
-
         return Command::SUCCESS;
     }
 
-    protected function runProcess(InputInterface $input, OutputInterface $output): void {
-        $output->writeln('<fg=white>timeout:</> <fg=yellow>' . $this->configRepository->getLongpullTimeout() . ' second</>');
-        $callback = function () use ($input, $output) {
-            $this->runItem($input, $output);
-        };
-        $loop = Loop::get();
-        $loop->addPeriodicTimer(0, $callback);
-        $loop->run();
-    }
-
-    public function runItem(InputInterface $input, OutputInterface $output) {
+    protected function runLoopItem(InputInterface $input, OutputInterface $output): void
+    {
         if (EnvHelper::isDebug()) {
             $output->writeln('<fg=white>wait...</>');
         }
@@ -68,9 +61,9 @@ class LongPullCommand extends Command
             //$output->writeln('<fg=green>has updates</>');
             foreach ($updates as $update) {
 //                    dd($update['message']['from']['id']);
-                if(!empty($update['message'])) {
+                if (!empty($update['message'])) {
                     $line = 'message ' . $update['update_id'] . ' from ' . $update['message']['chat']['id'];
-                    if(isset($update['message']['chat']['username'])) {
+                    if (isset($update['message']['chat']['username'])) {
                         $line .= ' (@' . $update['message']['chat']['username'] . ')';
                     } elseif ($update['message']['chat']['first_name']) {
                         $line .= ' (' . $update['message']['chat']['first_name'] . ')';
@@ -81,9 +74,9 @@ class LongPullCommand extends Command
                         $output->writeln('<fg=green>OK</>');
                     } catch (\Throwable $e) {
                         $this->longPullService->setHandled($update);
-                        $output->writeln('<fg=red>FAIL '.$e->getMessage().'</>');
+                        $output->writeln('<fg=red>FAIL ' . $e->getMessage() . '</>');
                     }
-                } elseif(!empty($update['channel_post'])) {
+                } elseif (!empty($update['channel_post'])) {
                     $line = 'channel post ' . $update['update_id'] . ' from ' . $update['channel_post']['chat']['id'];
                     if ($update['channel_post']['chat']['title']) {
                         $line .= ' (' . $update['channel_post']['chat']['title'] . ')';
